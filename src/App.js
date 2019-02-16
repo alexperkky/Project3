7518c0fe43d63fc9d1c0c71b47ac1c20fc7d5f9f
pbang <phillipbang@gmail.com>
2019-02-16 10:47:24 -0800

    react commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import welcomePage from "./pages/welcomePage"
+import loginPage from "./pages/loginPage"
+import signupPage from "./pages/signupPage"
+import noteBoard from "./pages/noteBoard"
 
 function App() {
   return(
     <Router>
+      <div>
       <Route exact path = "/" component = {welcomePage} />
+      <Route exact path = "/login" component = {loginPage} />
+      <Route exact path = "/signup" component = {signupPage} />
+      <Route exact path = "/noteboard" component = {noteBoard} />
+      </div>
     </Router>
   )
 }
