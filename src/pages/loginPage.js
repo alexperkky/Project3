748a7414cd31be09ccde74ffa1cbb6bddd8f920e
Chinzorig Boldmyagmar <chinzorig88@yahoo.com>
2019-02-20 11:23:59 -0800

    updated login and sign up pages

--- src/pages/loginPage.js
+++ src/pages/loginPage.js
@@ -7,21 +7,22 @@ import Container from "../components/container";
 function loginPage() {
   return (
     <div>
-      <Container>
-        <Wrapper>
-          <Jumbotron>
-            <h1 className="display-3">VoiceStickies</h1>
-            <p className="lead">
-              An app that records your voice and translates to text
-            </p>
-            <p className="lead">
-              Please make an account or sign-in to an existing
-            </p>
-          </Jumbotron>
-        </Wrapper>
-      </Container>
-      <Footer />
-    </div>
+
+      <form method="post" action="/login">
+        <h2 class="sr-only">Login Form</h2>
+        <div class="form-group">
+          <input class="form-control" name="username" placeholder="Email" id="loginEmail" />
+            </div>
+          <div class="form-group">
+            <input class="form-control" type="password" name="password" placeholder="Password" id="loginPassword" />
+            </div>
+            <div class="form-group">
+              <button class="btn btn-primary btn-block" type="submit" id="login">Log In</button>
+            </div>
+      </form>
+
+    <Footer />
+    </div >
   );
 }
 
