7a7947f860c6be772e8ed6f29c5e347ffe0b3870
Alexperkky <alex.perkinson5@gmail.com>
2019-02-20 15:07:48 -0800

    Push up remaining componenets, app not functional

---
+++ src/components/stickyNote/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../stickyNote/style.css";
+
+const stickyNote = (props) => {
+  return (
+    <div>
+      <li>
+        
+          <h2>Title #1</h2>
+          <p>Text Content #1</p>
+        
+      </li>
+    </div>
+  );
+};
+
+export default stickyNote;
