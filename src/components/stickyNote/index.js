7df8c5ff44b2d17713ba3c630a0878de1b769ca5
Alexperkky <alex.perkinson5@gmail.com>
2019-02-20 15:17:55 -0800

    Updated

--- src/components/stickyNote/index.js
+++ src/components/stickyNote/index.js
@@ -5,10 +5,13 @@ const stickyNote = (props) => {
   return (
     <div>
       <li>
-        
-          <h2>Title #1</h2>
-          <p>Text Content #1</p>
-        
+      '<div class="note">' +
+  '<a href="javascript:;" class="button remove">X</a>' +
+  '<div class="note_cnt">' +
+  '<textarea class="title" placeholder="Enter note title"></textarea>' +
+  '<textarea class="cnt" placeholder="Enter note description here"></textarea>' +
+  "</div> " +
+  "</div>";
       </li>
     </div>
   );
